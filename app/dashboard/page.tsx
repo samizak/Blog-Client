599074fc3894ef21514e74cbd786f7284819b815
Sami Zakir Ahmed <54m1z4k@gmail.com>
2023-07-08 14:39:51 +0100

    Disabled caching when fetching

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,7 +7,7 @@ import ArticleCard from "../components/ArticleCard";
 dotenv.config();
 
 async function getData() {
-  const res = await fetch(process.env.NEXT_PUBLIC_API_URI + `/api/v1/blogs`);
+  const res = await fetch(process.env.NEXT_PUBLIC_API_URI + `/api/v1/blogs`, { cache: "no-store" });
   if (!res.ok) throw new Error("Failed to fetch data");
   const json = await res.json();
   return json.blogPosts;
