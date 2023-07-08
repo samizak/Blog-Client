5ba6e70b339bc4db60ce2782af41024cf37cf693
Sami Zakir Ahmed <54m1z4k@gmail.com>
2023-07-08 14:40:02 +0100

    minor changes

--- app/components/ArticleCard.tsx
+++ app/components/ArticleCard.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 export default function ArticleCard({ blogArticle }: { blogArticle: IBlogArticle }) {
   return (
-    <div className="flex flex-col justify-between w-56 h-56 px-6 py-8 text-xs text-center align-middle shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] rounded-lg">
+    <div className="flex flex-col justify-between w-72 h-72 px-6 py-8 text-sm text-center align-middle shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] rounded-lg">
       <div className="flex flex-col gap-2">
         <h1 className="text-2xl font-bold">{blogArticle.title}</h1>
         <p className="font-light line-clamp-3">{blogArticle.content}</p>
@@ -17,7 +17,6 @@ export default function ArticleCard({ blogArticle }: { blogArticle: IBlogArticle
 
         <Link
           className="w-full p-1 bg-gray-200"
-          // className="flex px-3 py-2 mt-4 text-sm text-white align-middle transition-colors bg-green-600 rounded cursor-pointer hover:bg-green-800 w-fit"
           href={{
             pathname: "/article",
             query: { id: blogArticle._id },
@@ -29,5 +28,3 @@ export default function ArticleCard({ blogArticle }: { blogArticle: IBlogArticle
     </div>
   );
 }
-
-async function ViewPost(blogArticle: IBlogArticle) {}
